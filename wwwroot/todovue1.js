8ddd7280836cee11ee58659891b29df92b93bf4e
inec <luyege+github@gmail.com>
2018-07-10 00:13:23 -0500

    'workpartial'

--- wwwroot/todovue1.js
+++ wwwroot/todovue1.js
@@ -2,11 +2,23 @@ console.log('tt');
 new Vue({
 		el: '#app',
 		data: {
-		  message: 'L-26 Hello Vue.js!'
-		},
+         todos: [ { "id": 1, "order": 2, "title": "tst332", "url": "http://localhost:5000/v1/todos/1", "completed": false }, { "id": 2, "order": 2, "title": "tst2", "url": "http://localhost:5000/v1/todos/2", "completed": false } ],
+
+           message: 'L-26 Hello Vue.js!'
+        },
+  created: function () {
+    // `this` points to the vm instance
+    Vue.http.get("/v1/todos").then(response => {
+       // this.todos = response.body;
+    });
+    console.log('a is: ' + this.todos.length)
+  },computed: {
+    filteredTodos: function () {
+      return filters[this.visibility](this.todos)
+    }},
   mounted () {
     axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.message  = response))
+     .get('/v1/todos')
+     .then(response => (this.message  = response.data)) // .get('https://api.coindesk.com/v1/bpi/currentprice.json')
   }
 	  })
