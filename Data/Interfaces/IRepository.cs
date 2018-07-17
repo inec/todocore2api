118bc877683eb6d3ec7553f595194e2b2750d437
inec <luyege+github@gmail.com>
2018-07-16 23:33:05 -0500

    'test'

---
+++ Data/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace todo_core_webapi.Data.Interfaces
+{
+	public interface IRepository<T> where T: class 
+    {
+IEnumerable<T> GetAll();
+          //  Task<IEnumerable<T>> GetAll();
+
+		Task<T> Get(int id);
+
+		Task<Todo> Add(Todo todo);
+
+		Task<Todo> Update(int id, Todo todo);
+
+		Task Delete(int id);
+
+		Task Clear();
+    }
+}
